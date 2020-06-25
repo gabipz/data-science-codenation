c3fe5b312a0a067fe8d983e0c6ecd113a83b002d
gabipz <gabriela.pss@hotmail.com>
2020-06-25 15:19:36 -0300

    fix: markdown

--- 00-classes/week-02/aula_2_Streamlit_Pandas.py
+++ 00-classes/week-02/aula_2_Streamlit_Pandas.py
@@ -6,13 +6,16 @@ def main():
     st.title('Streamlit and Pandas')
     file = st.file_uploader('Upload your file', type='csv') #you can use 'pd.read_csv('file_name.csv')' direct 
     if file is not None:
+        st.markdown('Slider')
         slider = st.slider('Values', 1,100)
-        df = pd.read_csv('IRIS.csv')
+        df = pd.read_csv(file)
         st.dataframe(df.head(slider))
-        st.markdown('Table format')
+        st.markdown('Slider: table format')
         st.table(df.head(slider))
+        st.markdown('Dataframe columns')
         st.write(df.columns)
-        st.table(df.groupby('species')['petal_width'].mean())
+        st.markdown('Mean')  
+        st.table(df.groupby('species')['petal_width'].mean()) #to file 'IRIS.csv'
     
 if __name__ == '__main__':
     main()
